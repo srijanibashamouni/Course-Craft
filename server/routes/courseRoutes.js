4575980853c23c85aa60760455ec49d338924bf5
Srijani Bashamouni <srijanibashamouni265@gmail.com>
2026-09-24 14:33:17 +0530

    complete code

--- server/routes/courseRoutes.js
+++ server/routes/courseRoutes.js
@@ -1,40 +1,15 @@
-const express = require('express');
+const  express = require('express')
+const courseRoute=express.Router()
 
-const { protect, authorize } = require('../middleware/authMiddleware');
+const {protect,authorize} = require('../middleware/authMiddleware')
+const { getCourse, createCourse,updateCourse,deleteCourse,getCourseById} = require('../controllers/courseController')
 
-const {
-    getCourses,
-    createCourse,
-    updateCourse,
-    deleteCourse,
-    getCourseById
-} = require('../controllers/courseController');
+courseRoute.get("/",getCourse)
+courseRoute.post("/",protect,authorize('instructor','admin'),createCourse)
+courseRoute.get("/:id",getCourseById)
 
-const courseRoute = express.Router();
+courseRoute.put("/:id",protect,authorize('instructor','admin'),updateCourse)
+courseRoute.delete("/:id",protect,authorize('instructor','admin'),deleteCourse)
 
-courseRoute.get('/', getCourses);
 
-courseRoute.post(
-    '/',
-    protect,
-    authorize('instructor', 'admin'),
-    createCourse
-);
-
-courseRoute.get('/:id', getCourseById);
-
-courseRoute.put(
-    '/:id',
-    protect,
-    authorize('instructor', 'admin'),
-    updateCourse
-);
-
-courseRoute.delete(
-    '/:id',
-    protect,
-    authorize('instructor', 'admin'),
-    deleteCourse
-);
-
-module.exports = courseRoute;
+module.exports = courseRoute
